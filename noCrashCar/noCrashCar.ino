eec8a9e7fac43f3a564146259791a106978df832
Robert Brandt <Robert Brandt>
2016-12-04 08:53:14 -0500

    add comments

--- noCrashCar/noCrashCar.ino
+++ noCrashCar/noCrashCar.ino
@@ -1,5 +1,23 @@
 #include "MeOrion.h"
 
+/*
+This code is for an Orion Makeblock (Arduino Uno) board.
+My son and I modified a $12 RC car by removing it's remove control
+leaving us with the chassis, wheels and two motors.
+One motor drives the rear wheels, forward/reverse/stop.
+The other drives the front wheels, left/right/center.
+We used an ultrasonic sensor mounted to the front to
+give a signal when it's going to hit an obstacle.
+When it detects an obstacle it stops, turns the wheel
+and goes in reverse, then forward straight again (close to a 90deg turn).
+My son added a buzzer in reverse all by himself by
+refering to the example scripts.
+With the top off the car the MakeBlock battery pack and Orion board
+sit on top of the chassis. We used zip ties to mount the ultrasonic
+detector to the front. The motor drivers we used paper clips to wire
+it up (real hack there, should be shielded wires).
+*/
+
 MeUltrasonicSensor ultraSensor(PORT_8); /* Ultrasonic module can ONLY be connected to port 3, 4, 6, 7, 8 of base shield. */
 
 MeDCMotor motor1(M1);
@@ -54,3 +72,4 @@ void loop() {
   }
   delay(100);
 }
+
